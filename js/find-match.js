06e4afff7dccb923d8f4f8712cedfd5cf5376b4a
sanjeet <sanjeet.kumar@twinhealth.com>
2022-02-10 08:56:16 +0530

    wip

---
+++ js/find-match.js
@@ -0,0 +1,114 @@
+// document.getElementById("dataListLeft").style.display = "none";
+// document.getElementById("dataListRight").style.display = "none";
+
+// onchange of centre
+function handleChangeCentreLeft(event) {
+	// var value = event.target.dataset.value;
+	// console.log(value);
+
+	document.getElementById("emptyListLeft").style.display = "none";
+	document.getElementById("dataListLeft").style.display = "block";
+}
+
+// onchange of classroom
+function handleChangeClassroomLeft(event) {
+	// var value = event.target.dataset.value;
+	// console.log(value);
+
+	document.getElementById("emptyListLeft").style.display = "none";
+	document.getElementById("dataListLeft").style.display = "block";
+}
+
+// onchange of days
+function handleChangeDaysLeft(event) {
+	var value = event.target.dataset.value;
+	console.log(value);
+
+	document.getElementById("emptyListLeft").style.display = "none";
+	document.getElementById("dataListLeft").style.display = "block";
+}
+
+// onchange of centre
+function handleChangeCentreRight(event) {
+	// var value = event.target.dataset.value;
+	// console.log(value);
+
+	document.getElementById("emptyListRight").style.display = "none";
+	document.getElementById("dataListRight").style.display = "block";
+}
+
+// onchange of classroom
+function handleChangeClassroomRight(event) {
+	// var value = event.target.dataset.value;
+	// console.log(value);
+
+	document.getElementById("emptyListRight").style.display = "none";
+	document.getElementById("dataListRight").style.display = "block";
+}
+
+// onchange of days
+function handleChangeDaysRight(event) {
+	var value = event.target.dataset.value;
+	console.log(value);
+
+	document.getElementById("emptyListRight").style.display = "none";
+	document.getElementById("dataListRight").style.display = "block";
+}
+
+// for custom select options
+for (const dropdown of document.querySelectorAll(".select_wrapper")) {
+	dropdown.addEventListener("click", function () {
+		this.querySelector(".select_container").classList.toggle("open");
+	});
+}
+
+for (const option of document.querySelectorAll(".select_custom_option")) {
+	option.addEventListener("click", function () {
+		if (!this.classList.contains("selected")) {
+			const selectedEle = this.parentNode.querySelector(
+				".select_custom_option.selected"
+			);
+			if (selectedEle) {
+				selectedEle.classList.remove("selected");
+			}
+			this.classList.add("selected");
+
+			this.closest(".select_container")
+				.querySelector(".select__trigger")
+				.classList.add("selected");
+			this.closest(".select_container").querySelector(
+				".select__trigger span"
+			).textContent = this.textContent;
+		}
+	});
+}
+
+document.addEventListener("click", function (e) {
+	for (const select of document.querySelectorAll(".select_container")) {
+		if (!select.contains(e.target)) {
+			select.classList.remove("open");
+		}
+	}
+});
+
+const selectedRows = document.querySelectorAll(".row_select");
+
+for (let i = 0; i < selectedRows.length; i++) {
+	selectedRows[i].addEventListener(
+		"click",
+		function () {
+			// multi selection
+			// this.classList.toggle("selected_row");
+
+			// single selection
+			const selectedEle = this.parentNode.querySelector(".selected_row");
+			// console.log("selectedEle", selectedEle);
+			if (selectedEle) {
+				// console.log("removed");
+				selectedEle.classList.remove("selected_row");
+			}
+			this.classList.add("selected_row");
+		},
+		false
+	);
+}
